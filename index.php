8324e5c39ae10c2f9cf518da6af72dece37da318
FrancescoLunesu <francesco.lunesu@outlook.it>
2020-09-17 18:21:22 +0200

    Primo commit: creazione header

---
+++ index.php
@@ -0,0 +1,44 @@
+<!-- Riscrivere questa pagina del sito google https://policies.google.com/faq.
+Ci sono diverse domande con relative risposte.
+Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP. -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Domande frequenti - Privacy e termini - Google</title>
+</head>
+<body>
+
+    <header>
+        <div class="top">
+            <div class="google-logo">
+                <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_74x24dp.png" alt="Google Logo">
+            </div>
+            <div>
+                <h3>Privacy e termini</h3>
+            </div>
+        </div>
+        <div class="navbar">
+            <ul>
+                <li>Introduzione</li>
+                <li>Norme sulla privacy</li>
+                <li>Termini di servizio</li>
+                <li>Tecnologie</li>
+                <li>Domande Frequenti</li>
+            </ul>
+        </div>
+    </header>
+    <hr>
+
+    <main>
+        <div class="container">
+
+        </div>
+    </main>
+
+</body>
+</html>
